13550d1f65938cb72c9b1f48f5d00ab1c4363478
FrancoMRuiz <x.francoruiz@gmail.com>
2021-05-19 02:13:38 -0300

    fix warnings

--- src/components/Main/SidebarChannel/SidebarChannel.js
+++ src/components/Main/SidebarChannel/SidebarChannel.js
@@ -1,13 +1,7 @@
 import React from "react";
 import {
-  NameChannel,
   SidebarChannelContainer,
-  SidebarChannelHeader,
 } from "./styles";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faChevronDown,
-} from "@fortawesome/free-solid-svg-icons";
 import { SidebarFooter } from "./SidebarFooter/SidebarFooter";
 import { Channel } from "./Channel/Channel";
 import { SidebarHeader } from "./SidebarHeader/SidebarHeader";
