7012f75008c43f5c84bfd06a5dccf7a9cf8adc48
FrancoMRuiz <x.francoruiz@gmail.com>
2021-05-20 19:53:55 -0300

    change theme colors

--- src/styles/globalTheme.js
+++ src/styles/globalTheme.js
@@ -1,13 +1,13 @@
 export const theme = {
   COLORS: {
     white: "#ffffff",
-    strongGray: "#23272a",
-    gray: "#2c2f33",
-    mediumGray: "#3C3C3C",
-    lightGray: "#99aab5",
+    strongGray: "#202225",
+    gray: "#2f3136",
+    mediumGray: "#36393f",
+    lightGray: "#b9bbbe",
     blue: "#7289da",
     black: "#212121",
-    footer: "#292929",
+    footer: "#292b2f",
     green: "#25d366",
   },
 };
