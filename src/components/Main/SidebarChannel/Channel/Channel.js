32a163b9ecc9902e412e998ce618823b490428cf
FrancoMRuiz <x.francoruiz@gmail.com>
2021-05-22 02:10:19 -0300

    install react-router

--- src/components/Main/SidebarChannel/Channel/Channel.js
+++ src/components/Main/SidebarChannel/Channel/Channel.js
@@ -5,7 +5,7 @@ import { ChannelHeader } from "../../../ChannelHeader/ChannelHeader";
 
 export const Channel = () => {
   const channelHeaders = [{ name: "THE OFFICE" }];
-  const channels = [{ name: "welcome" }, { name: "general-chat" }];
+  const channels = [{ name: "welcome" }, { name: "general-chat" }, { name: "music" }];
 
   return (
     <ChannelContainer>
