e260a346cd41330eb046445e4d89172f22356932
FrancoMRuiz <x.francoruiz@gmail.com>
2021-05-30 03:13:39 -0300

    add StreamingHeader component

--- src/components/Main/StreamingScreen/StreamingScreen.js
+++ src/components/Main/StreamingScreen/StreamingScreen.js
@@ -1,6 +1,15 @@
 import React from "react";
 import {
+  ChannelName,
+  HeaderOptionsContainer,
+  LiveLabel,
+  QualityLabel,
+  QualityVideoContainer,
+  StreamerName,
+  StreamingContainer,
+  StreamingHeaderContainer,
   StreamingScreenContainer,
+  StreamingUsernameContainer,
   UsersConectedContainer,
   UsersConectedImg,
   UsersConectedImgContainer,
@@ -11,6 +20,13 @@ import Elon from "../../../assets/elon.jpg";
 import Bezos from "../../../assets/bezos.jpg";
 import Mark from "../../../assets/mark.jpg";
 import ElonVideo from "../../../assets/video/elon.mp4";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBorderNone,
+  faEllipsisH,
+  faInbox,
+  faVolumeDown,
+} from "@fortawesome/free-solid-svg-icons";
 
 export const StreamingScreen = () => {
   const streamingUsers = [
@@ -20,18 +36,49 @@ export const StreamingScreen = () => {
   ];
 
   return (
-    <StreamingScreenContainer>
-      <VideoStreaming width={1275} autoPlay muted>
-        <source src={ElonVideo} type="video/mp4" />
-      </VideoStreaming>
-      <UsersConectedContainer>
-        {streamingUsers.map((s, i) => (
-          <UsersConectedImgContainer className={s.color} key={i}>
-            <UsersConectedImg src={s.img} />
-            <UsersConectedName className="user__name">{s.name}</UsersConectedName>
-          </UsersConectedImgContainer>
-        ))}
-      </UsersConectedContainer>
-    </StreamingScreenContainer>
+    <StreamingContainer>
+      <StreamingHeaderContainer>
+        <StreamingUsernameContainer>
+          <ChannelName>
+            <FontAwesomeIcon
+              icon={faVolumeDown}
+              className="fontawesome__channel"
+            />{" "}
+            team-work
+          </ChannelName>
+          <StreamerName>ElonX</StreamerName>
+        </StreamingUsernameContainer>
+        <HeaderOptionsContainer>
+          <QualityVideoContainer>
+            <QualityLabel>720p 30FPS</QualityLabel>
+            <LiveLabel>LIVE</LiveLabel>
+          </QualityVideoContainer>
+          <FontAwesomeIcon
+            icon={faBorderNone}
+            className="fontawesome__channel"
+          />
+          <FontAwesomeIcon
+            icon={faEllipsisH}
+            className="fontawesome__channel"
+          />
+          <FontAwesomeIcon icon={faInbox} className="fontawesome__channel" />
+        </HeaderOptionsContainer>
+      </StreamingHeaderContainer>
+      <StreamingScreenContainer>
+        <VideoStreaming width={1275} autoPlay muted>
+          <source src={ElonVideo} type="video/mp4" />
+        </VideoStreaming>
+        <UsersConectedContainer>
+          {streamingUsers.map((s, i) => (
+            <UsersConectedImgContainer className={s.color} key={i}>
+              <UsersConectedImg src={s.img} />
+              <UsersConectedName className="user__name">
+                {s.name}
+              </UsersConectedName>
+            </UsersConectedImgContainer>
+          ))}
+        </UsersConectedContainer>
+      </StreamingScreenContainer>
+    </StreamingContainer>
   );
 };
--- src/components/Main/StreamingScreen/styles.js
+++ src/components/Main/StreamingScreen/styles.js
@@ -1,5 +1,76 @@
 import styled from "styled-components";
 
+export const StreamingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: black;
+  height: 100%;
+  cursor: pointer;
+
+
+  .fontawesome__channel {
+    color: ${props => props.theme.COLORS.lightGray};
+  }
+`;
+
+export const StreamingHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 5%;
+`;
+
+export const StreamingUsernameContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ChannelName = styled.p`
+  margin: 0;
+  color: ${props => props.theme.COLORS.white};
+  padding-left: 15px;
+  padding-right: 15px;
+`;
+
+export const StreamerName = styled.p`
+  margin: 0;
+  color: ${props => props.theme.COLORS.lightGray};
+  padding-left: 15px;
+  border-left: 1px solid ${props => props.theme.COLORS.gray};
+`;
+
+export const HeaderOptionsContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 20%;
+`;
+
+export const QualityVideoContainer = styled.div`
+  display: flex;
+`;
+
+export const QualityLabel = styled.p`
+  color: ${props => props.theme.COLORS.white};
+  background-color: ${props => props.theme.COLORS.gray};
+  margin: 0;
+  padding: 2px 5px 2px 5px;
+  font-weight: bold;
+  font-size: 13px;
+  border-top-left-radius: 10px;
+  border-bottom-left-radius: 10px;
+`;
+
+export const LiveLabel = styled.p`
+  color: ${props => props.theme.COLORS.white};
+  background-color: ${props => props.theme.COLORS.red};
+  margin: 0;
+  padding: 2px 8px 2px 5px;
+  font-weight: bold;
+  font-size: 13px;
+  border-top-right-radius: 10px;
+  border-bottom-right-radius: 10px;
+`;
+
 export const StreamingScreenContainer = styled.div`
   display: flex;
   align-items: center;
@@ -18,7 +89,6 @@ export const VideoStreaming = styled.video`
   border-radius: 5px;
   margin-left: 10px;
   margin-right: 10px;
-  cursor: pointer;
 `;
 
 export const UsersConectedContainer = styled.div`
