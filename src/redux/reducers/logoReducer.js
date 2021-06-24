0c32114884e0182ce4fbfc7a241a2669b023c35a
FrancoMRuiz <x.francoruiz@gmail.com>
2021-06-24 03:11:10 -0300

    add logoReducer

---
+++ src/redux/reducers/logoReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  logo: [],
+  loading: false,
+  error: false,
+};
+
+export default function logoReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
