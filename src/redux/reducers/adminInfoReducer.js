81f1ad0794ec9d56d570d8a0365d1e9bfad28c54
FrancoMRuiz <x.francoruiz@gmail.com>
2021-06-27 02:27:57 -0300

    add adminInfoReducer

---
+++ src/redux/reducers/adminInfoReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  adminInfo: [],
+  loading: false,
+  error: false,
+};
+
+export default function adminInfoReducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
