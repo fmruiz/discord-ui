663268b28b12da566219bb344c3eae7be1933a40
FrancoMRuiz <x.francoruiz@gmail.com>
2021-12-05 20:43:55 -0300

    add initial meme publication

--- src/components/Memes/MemesPublication/index.js
+++ src/components/Memes/MemesPublication/index.js
@@ -2,28 +2,41 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getInitialMemes } from "../../../redux/actions/memesActions";
 import { PublicationContainer, UserImg, UsernameText } from "../../common";
-import { Container, UserInformationContainer, UserPublication } from "./styles";
+import {
+  Container,
+  MemeImage,
+  TitleImage,
+  UserInformationContainer,
+  UserPublication,
+} from "./styles";
 
 const MemesPublication = () => {
   const dispatch = useDispatch();
-  // logo state
-  const { adminInfo } = useSelector((state) => state.adminInfo);
 
   useEffect(() => {
     dispatch(getInitialMemes());
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  // from redux states
+  const { adminInfo } = useSelector((state) => state.adminInfo);
+  const { initialMeme } = useSelector((state) => state.memes);
+
+  console.log(initialMeme);
 
   return (
     <Container>
-      <PublicationContainer>
-        <UserPublication>
-          <UserImg src={adminInfo.logo.url} />
-          <UserInformationContainer>
-            <UsernameText>{adminInfo.name}</UsernameText>
-          </UserInformationContainer>
-        </UserPublication>
-      </PublicationContainer>
+      {initialMeme.map((meme, i) => (
+        <PublicationContainer key={i}>
+          <UserPublication>
+            <UserImg src={adminInfo.logo.url} />
+            <UserInformationContainer>
+              <UsernameText>{adminInfo.name}</UsernameText>
+              <TitleImage>{`"${meme.title}"`}</TitleImage>
+              <MemeImage src={meme.images.downsized.url} alt={meme.title} />
+            </UserInformationContainer>
+          </UserPublication>
+        </PublicationContainer>
+      ))}
     </Container>
   );
 };
--- src/components/Memes/MemesPublication/styles.js
+++ src/components/Memes/MemesPublication/styles.js
@@ -14,3 +14,12 @@ export const UserInformationContainer = styled.div`
   flex-direction: column;
   margin-left: 20px;
 `;
+
+export const TitleImage = styled.span`
+    color: #ffffff;
+    text-decoration: underline;
+    font-style: italic;
+    margin-bottom: 10px;
+`
+
+export const MemeImage = styled.img``
